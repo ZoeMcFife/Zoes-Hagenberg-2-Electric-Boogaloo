e1a8ffaa575a9b8e7a7b6dc61ea0fd56c3d26503
Zoe McFife <cmdrprotogen27@gmail.com>
2026-04-08 12:39:02 +0200

    dfgsdgd

--- Algorithmic Thinking/tasks/A03 - Snakes & Towers/Snake/snake/src/main/java/gay/fox/snake/SnakeApplication.java
+++ Algorithmic Thinking/tasks/A03 - Snakes & Towers/Snake/snake/src/main/java/gay/fox/snake/SnakeApplication.java
@@ -12,8 +12,8 @@ public class SnakeApplication extends Application {
     public void start(Stage stage) throws IOException
     {
         FXMLLoader fxmlLoader = new FXMLLoader(SnakeApplication.class.getResource("snake-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 600, 600);
+        stage.setTitle("Snake!");
         stage.setScene(scene);
         stage.show();
     }
