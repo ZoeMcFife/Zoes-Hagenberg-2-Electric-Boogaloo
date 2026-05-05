27e28397de5258f45cfef0e8cc03926671eaf907
Zoe McFife <cmdrprotogen27@gmail.com>
2026-05-05 14:30:02 +0200

    boring

--- Algorithmic Thinking/exercises/2026-05-05/ue10_dao_3layer_starter/src/main/java/at/fhooe/ald/pokemon/data_access/jdbc/JdbcPokemonDAO.java
+++ Algorithmic Thinking/exercises/2026-05-05/ue10_dao_3layer_starter/src/main/java/at/fhooe/ald/pokemon/data_access/jdbc/JdbcPokemonDAO.java
@@ -8,9 +8,12 @@ import org.springframework.stereotype.Repository;
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+
 
 @Repository
 public class JdbcPokemonDAO implements PokemonDAO
@@ -30,9 +33,10 @@ public class JdbcPokemonDAO implements PokemonDAO
     @Override
     public void save(Pokemon pokemon)
     {
-        String sql = """
+        String sql =
+                """
                    INSERT INTO Pokemon (pokedex_number, name, type_1, type_2, hp, attack, defense, speed)
-                   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);
+                   VALUES (?, ?, ?, ?, ?, ?, ?, ?);
                 """;
 
         try(Connection conn = openConnection())
@@ -44,10 +48,9 @@ public class JdbcPokemonDAO implements PokemonDAO
             ps.setString(3, pokemon.getType1());
             ps.setString(4, pokemon.getType2());
             ps.setInt(5, pokemon.getHp());
-            ps.setInt(6, pokemon.getHp());
-            ps.setInt(7, pokemon.getAttack());
-            ps.setInt(8, pokemon.getDefense());
-            ps.setInt(9, pokemon.getSpeed());
+            ps.setInt(6, pokemon.getAttack());
+            ps.setInt(7, pokemon.getDefense());
+            ps.setInt(8, pokemon.getSpeed());
             ps.executeUpdate();
         }
         catch (SQLException e)
@@ -59,24 +62,127 @@ public class JdbcPokemonDAO implements PokemonDAO
     @Override
     public Optional<Pokemon> findByPokedexNumber(int pokedexNumber)
     {
-        return Optional.empty();
+        String sql =
+                """
+                    SELECT * FROM pokemon
+                    WHERE pokedex_number = ?;
+                """;
+
+        try (Connection conn = openConnection())
+        {
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setInt(1, pokedexNumber);
+
+            try (ResultSet rs = ps.executeQuery())
+            {
+                return rs.next() ? Optional.of(mapRow(rs)) : Optional.empty();
+            }
+            catch (SQLException e)
+            {
+                throw new SQLException("Could not find Pokemon", e);
+            }
+
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException("Could not find Pokemon", e);
+        }
     }
 
     @Override
     public List<Pokemon> findAll()
     {
-        return List.of();
+        String sql =
+                """
+                    SELECT * FROM pokemon
+                    ORDER BY pokedex_number;
+                """;
+        List<Pokemon> pokemons = new ArrayList<>();
+
+        try (Connection conn = openConnection();
+             PreparedStatement ps = conn.prepareStatement(sql);
+             ResultSet rs = ps.executeQuery();)
+        {
+            while (rs.next())
+            {
+                pokemons.add(mapRow(rs));
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException("Could not find Pokemon", e);
+        }
+
+        return pokemons;
     }
 
     @Override
     public void update(Pokemon pokemon)
     {
+        String sql =
+                """
+                   UPDATE Pokemon
+                   SET name = ?, type_1 = ?, type_2 = ?, hp = ?, attack = ?,  defense = ?, speed = ?
+                   WHERE pokedex_number = ?;
+                """;
 
+        try(Connection conn = openConnection())
+        {
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setString(1, pokemon.getName());
+            ps.setString(2, pokemon.getType1());
+            ps.setString(3, pokemon.getType2());
+            ps.setInt(4, pokemon.getHp());
+            ps.setInt(5, pokemon.getAttack());
+            ps.setInt(6, pokemon.getDefense());
+            ps.setInt(7, pokemon.getSpeed());
+            ps.setInt(8, pokemon.getPokedexNumber());
+
+            ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException("Could not save Pokemon", e);
+        }
     }
 
     @Override
     public void delete(int pokedexNumber)
     {
+        String sql =
+                """
+                   DELETE FROM pokemon
+                   WHERE pokedex_number = ?;
+                """;
 
+        try(Connection conn = openConnection())
+        {
+            PreparedStatement ps = conn.prepareStatement(sql);
+
+            ps.setInt(1, pokedexNumber);
+
+            ps.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException("Could not delete Pokemon", e);
+        }
+    }
+
+
+    private Pokemon mapRow(ResultSet rs) throws SQLException
+    {
+        return new Pokemon
+                (
+                    rs.getInt("pokedex_number"),
+                    rs.getString("name"),
+                    rs.getString("type_1"),
+                    rs.getString("type_2"),
+                    rs.getInt("hp"),
+                    rs.getInt("attack"),
+                    rs.getInt("defense"),
+                    rs.getInt("speed")
+                );
     }
 }
